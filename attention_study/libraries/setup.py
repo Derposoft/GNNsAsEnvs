8f15e53470b66d64cd7847260812dbe059758273
Viswanath Chadalapaka <viswanath.chadalapaka@gmail.com>
2022-01-14 18:08:29 -0800

    +naming changes

--- attention_study/libraries/setup.py
+++ attention_study/libraries/setup.py
@@ -1,7 +1,7 @@
 import setuptools
 from setuptools import setup
 
-setup(name='attention-learn-to-route',
+setup(name='attention_routing',
       version='0.1.1',
       description='A package required for this combinatorial opt RL study. More info on this specific package at the github repo; more info on this study in the README.',
       url='https://github.com/wouterkool/attention-learn-to-route.git',
