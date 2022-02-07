623c2c995e90e13b9e87eba40d6d115260f12892
derposoft <viswanath.chadalapaka@gmail.com>
2022-02-06 16:21:53 -0800

    +early stopping

--- attention_study/model/test_model.py
+++ attention_study/model/test_model.py
@@ -16,7 +16,7 @@ from attention_study.model.utils import embed_obs_in_map, load_edge_dictionary
 from sigma_graph.data.file_manager import set_visibility
 
 # 3rd party
-from attention_routing.nets.attention_model import AttentionModel
+from attention_routing.nets.attention_model import AttentionModel, set_decode_type
 from attention_routing.problems.tsp.problem_tsp import TSP
 from attention_routing.train import train_batch
 from attention_routing.utils.log_utils import log_values
@@ -170,13 +170,20 @@ TEST_SETTINGS = {
     'is_standalone': True, # are we training it in rllib, or standalone?
     'is_360_view': True, # can the agent see in all directions at once?
     'is_obs_embedded': False, # are our observations embedded into the graph?
-    'is_per_step': True, # do we optimize every step if True, or every episode if False?
+    'is_per_step': False, # do we optimize every step if True, or every episode if False?
+    'is_mask_in_model': False, # do we use the mask in the model or after the model?
+    'use_hardcoded_bl': True, # subtract off a hardcoded "baseline" value
 }
+MAXIMUM_THEORETICAL_REWARD = 25
+def get_cost_from_reward(reward):
+    return 1/(reward + 1e-3) # takes care of div by 0
 
 def optimize(optimizer, baseline, reward, ll):
     # set costs
-    model_cost = 1/(reward + 1e-3) # removes div by 0 error
+    model_cost = get_cost_from_reward(reward)
     bl_val, bl_loss = baseline.eval(attention_input, model_cost) #if bl_val is None else (bl_val, 0) # critic loss
+    if TEST_SETTINGS['use_hardcoded_bl']:
+        bl_val = get_cost_from_reward(MAXIMUM_THEORETICAL_REWARD)
     reinforce_loss = ((model_cost - bl_val) * ll).mean()
     loss = reinforce_loss + bl_loss
     # perform optimization step
@@ -184,8 +191,7 @@ def optimize(optimizer, baseline, reward, ll):
     loss.backward()
     grad_norms = clip_grad_norms(optimizer.param_groups, opts.max_grad_norm)
     optimizer.step()
-    return grad_norms
-    
+    return grad_norms, loss
 
 if __name__ == "__main__":
     # init config
@@ -202,7 +208,7 @@ if __name__ == "__main__":
         model, optimizer, baseline, lr_scheduler, tb_logger =\
             initialize_train_artifacts(opts)
         model.train()
-        model.set_decode_type("sampling")
+        set_decode_type(model, "sampling")
         
         # init training env
         training_env = Figure8SquadRLLib(outer_configs)
@@ -223,51 +229,57 @@ if __name__ == "__main__":
             training_env.reset();
             agent_node = training_env.team_red[training_env.learning_agent[0]].agent_node # 1-indexed value
             for step in range(episode_length):
-                # if we have a reward in a reinforcement learning scenario, train on that instead
+                # get model predictions
                 if TEST_SETTINGS['is_obs_embedded']:
                     attention_input = embed_obs_in_map(obs, training_env.map) # embed obs every time we get a new obs
-                cost, ll, log_ps = model(attention_input, acs_edges_dict, [agent_node-1], return_log_p=True)
-                if not total_ll:
-                    total_ll = ll
+                if TEST_SETTINGS['is_mask_in_model']:
+                    cost, ll, log_ps = model(attention_input, acs_edges_dict, [agent_node-1], return_log_p=True)
                 else:
-                    total_ll += ll
-                
-                # move_action decoding. get max prob moves from map
+                    cost, ll, log_ps = model(attention_input, return_log_p=True)
+                # mask model predictions with our graph edges
+                if not TEST_SETTINGS['is_mask_in_model']:
+                    node_exclude_list = np.array(list(range(attention_input.shape[1])))
+                    mask = [np.delete(node_exclude_list, list(acs_edges_dict[agent_node])) for agent_node in [agent_node-1]]
+                    for i in range(len(mask)):
+                        log_ps[i][mask[i]] = 0
+                # move_action decoding. get max prob moves from masked predictions
                 features = log_ps # set features for value branch later
                 transformed_features = features.clone()
                 transformed_features[transformed_features == 0] = -float('inf')
                 optimal_destination = torch.argmax(transformed_features, dim=1)
-                
-                # decode move/turn action
                 curr_loc = agent_node
                 next_loc = optimal_destination[0].item() + 1
                 move_action = training_env.map.g_acs.adj[curr_loc][next_loc]['action']
                 look_action = 1 # TODO!!!!!!!! currently uses all-way look
                 action = Figure8SquadRLLib.convert_multidiscrete_action_to_discrete(move_action, look_action)
-
                 # step through environment to update obs/rew and agent node
                 actions = {}
                 for a in training_env.learning_agent:
                     actions[str(a)] = action
                 obs, rew, done, _ = training_env.step(actions)
                 agent_node = training_env.team_red[training_env.learning_agent[0]].agent_node
-
-                # collect rewards
+                # collect rewards/losses
                 for a in training_env.learning_agent:
                     total_reward += rew[str(a)]
-                
+                if not total_ll:
+                    total_ll = ll
+                else:
+                    total_ll += ll
                 # optimize once per step
                 if TEST_SETTINGS['is_per_step']:
-                    grad_norms = optimize(optimizer, baseline, total_reward, total_ll)
+                    grad_norms, loss = optimize(optimizer, baseline, total_reward, total_ll)
                     # reset for next iteration
                     logged_reward += total_reward
                     logged_ll = total_ll if not logged_ll else logged_ll + total_ll
                     total_reward = 0
                     total_ll = None
+                # end episode if simulation is done
+                if done['__all__']:
+                    break
             
             # optimize once per episode
             if not TEST_SETTINGS['is_per_step']:
-                grad_norms = optimize(optimizer, baseline, total_reward / episode_length, total_ll / episode_length)
+                grad_norms, loss = optimize(optimizer, baseline, total_reward / episode_length, total_ll / episode_length)
                 # reset for next iteration
                 logged_reward = total_reward
                 logged_ll = total_ll
@@ -279,9 +291,9 @@ if __name__ == "__main__":
             # log step in tb for metrics
             #if episode % int(opts.log_step) == 0:
             if episode % 10 == 0:
-                logged_reward = torch.tensor(logged_reward)
-                log_values(torch.tensor(logged_reward, dtype=torch.float32), grad_norms, episode, episode, episode,
-                        logged_ll, logged_reward, 0, tb_logger, opts, mode="reward")
+                logged_reward = torch.tensor(logged_reward, dtype=torch.float32)
+                log_values(logged_reward, grad_norms, episode, episode, episode,
+                        logged_ll, loss, 0, tb_logger, opts, mode="reward")
             logged_reward = 0
             logged_ll = None
     else:
