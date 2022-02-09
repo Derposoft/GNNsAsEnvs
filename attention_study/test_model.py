d476d64961b542f26eeb35c0c433a0bc7e572d6f
derposoft <viswanath.chadalapaka@gmail.com>
2022-02-08 16:02:48 -0800

    +ready to easily be used by others; +some restructuring

--- attention_study/test_model.py
+++ attention_study/test_model.py
@@ -6,18 +6,15 @@ import os
 import torch
 import torch.optim as optim
 from tensorboard_logger import Logger as TbLogger
-import sys
 
 # our code
 from sigma_graph.envs.figure8.figure8_squad_rllib import Figure8SquadRLLib
-from attention_study.model.attention_policy import PolicyModel
 from attention_study.generate_baseline_metrics import parse_arguments, create_env_config, create_trainer_config
 from attention_study.model.utils import embed_obs_in_map, load_edge_dictionary
 from sigma_graph.data.file_manager import set_visibility
 
 # 3rd party
 from attention_routing.nets.attention_model import AttentionModel, set_decode_type
-from attention_routing.problems.tsp.problem_tsp import TSP
 from attention_routing.train import train_batch
 from attention_routing.utils.log_utils import log_values
 from attention_routing.nets.critic_network import CriticNetwork
@@ -29,6 +26,18 @@ from attention_routing.nets.pointer_network import PointerNetwork, CriticNetwork
 from attention_routing.utils import torch_load_cpu, load_problem
 from attention_routing.train import clip_grad_norms
 
+TEST_SETTINGS = {
+    'is_standalone': True, # are we training it in rllib, or standalone?
+    'is_360_view': True, # can the agent see in all directions at once?
+    'is_obs_embedded': False, # are our observations embedded into the graph?
+    'is_per_step': False, # do we optimize every step if True, or every episode if False?
+    'is_mask_in_model': False, # do we use the mask in the model or after the model?
+    'use_hardcoded_bl': True, # subtract off a hardcoded "baseline" value
+    'normalize_losses_rewards_by_ep_length': False, # divide losses/rewards/loglosses by ep length?
+    'episode_length': 20, # length of each episode
+    'num_episodes': 10000, # number of episodes to train for
+}
+MAXIMUM_THEORETICAL_REWARD = 25
 
 def initialize_train_artifacts(opts):
     '''
@@ -166,16 +175,6 @@ def initialize_train_artifacts(opts):
     return model, optimizer, baseline, lr_scheduler, tb_logger
 
 
-TEST_SETTINGS = {
-    'is_standalone': True, # are we training it in rllib, or standalone?
-    'is_360_view': True, # can the agent see in all directions at once?
-    'is_obs_embedded': False, # are our observations embedded into the graph?
-    'is_per_step': False, # do we optimize every step if True, or every episode if False?
-    'is_mask_in_model': False, # do we use the mask in the model or after the model?
-    'use_hardcoded_bl': True, # subtract off a hardcoded "baseline" value
-    'normalize_losses_rewards_by_ep_length': False, # divide losses/rewards/loglosses by ep length?
-}
-MAXIMUM_THEORETICAL_REWARD = 25
 def get_cost_from_reward(reward):
     return 1/(reward + 1e-3) # takes care of div by 0
 
@@ -194,9 +193,6 @@ def optimize(optimizer, baseline, reward, ll, num_steps=1):
     #reinforce_loss = -((model_cost - bl_val) * ll).mean()
     reinforce_loss = ((model_cost - bl_val) * ll).mean()
     loss = reinforce_loss + bl_loss
-    print(model_cost, bl_val, "reward_costs")
-    print(reinforce_loss, bl_loss, "train_losses", ll)
-    print(loss)
     # perform optimization step
     optimizer.zero_grad()
     loss.backward()
@@ -230,8 +226,8 @@ if __name__ == "__main__":
         
         # training loop
         print('training')
-        episode_length = 20
-        num_training_episodes = 10000
+        episode_length = TEST_SETTINGS['episode_length']
+        num_training_episodes = TEST_SETTINGS['num_episodes']
         total_reward = 0
         total_ll = None
         logged_reward = 0 # for logging
@@ -247,12 +243,14 @@ if __name__ == "__main__":
                     cost, ll, log_ps = model(attention_input, acs_edges_dict, [agent_node-1], return_log_p=True)
                 else:
                     cost, ll, log_ps = model(attention_input, return_log_p=True)
+                
                 # mask model predictions with our graph edges
                 if not TEST_SETTINGS['is_mask_in_model']:
                     node_exclude_list = np.array(list(range(attention_input.shape[1])))
                     mask = [np.delete(node_exclude_list, list(acs_edges_dict[agent_node])) for agent_node in [agent_node-1]]
                     for i in range(len(mask)):
                         log_ps[i][mask[i]] = 0
+                
                 # move_action decoding. get max prob moves from masked predictions
                 features = log_ps # set features for value branch later
                 transformed_features = features.clone()
@@ -263,12 +261,14 @@ if __name__ == "__main__":
                 move_action = training_env.map.g_acs.adj[curr_loc][next_loc]['action']
                 look_action = 1 # TODO!!!!!!!! currently uses all-way look
                 action = Figure8SquadRLLib.convert_multidiscrete_action_to_discrete(move_action, look_action)
+
                 # step through environment to update obs/rew and agent node
                 actions = {}
                 for a in training_env.learning_agent:
                     actions[str(a)] = action
                 obs, rew, done, _ = training_env.step(actions)
                 agent_node = training_env.team_red[training_env.learning_agent[0]].agent_node
+
                 # collect rewards/losses
                 for a in training_env.learning_agent:
                     total_reward += rew[str(a)]
@@ -276,6 +276,7 @@ if __name__ == "__main__":
                     total_ll = ll
                 else:
                     total_ll += ll
+                
                 # optimize once per step
                 if TEST_SETTINGS['is_per_step']:
                     grad_norms, loss = optimize(optimizer, baseline, total_reward, total_ll)
@@ -284,6 +285,7 @@ if __name__ == "__main__":
                     logged_ll = total_ll if not logged_ll else logged_ll + total_ll
                     total_reward = 0
                     total_ll = None
+                
                 # end episode if simulation is done
                 if done['__all__']:
                     break
