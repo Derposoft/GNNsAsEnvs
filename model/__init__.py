e4279741088d6c9873f20dad80a1a1f77b0a22fb
derposoft <viswanath.chadalapaka@gmail.com>
2022-08-12 20:15:33 -0700

    +added gat/pyg stuff again, but not functional

--- model/__init__.py
+++ model/__init__.py
@@ -1,14 +1,18 @@
 from ray.rllib.models.catalog import ModelCatalog
-#from model.altr_rllib import AltrPolicy
-# from model.gat_rllib import GATPolicy
+from model.gat_rllib import GATPolicy
 from model.graph_transformer_rllib import GraphTransformerPolicy
 from model.hybrid_rllib import HybridPolicy
 from model.fc_rllib import FCPolicy
 
 # register our model (put in an __init__ file later)
 # https://docs.ray.io/en/latest/rllib-models.html#customizing-preprocessors-and-models
-#ModelCatalog.register_custom_model("altr_policy", AltrPolicy)
-# ModelCatalog.register_custom_model("gat_policy", GATPolicy)
+ModelCatalog.register_custom_model("gat_policy", GATPolicy)
 ModelCatalog.register_custom_model("graph_transformer_policy", GraphTransformerPolicy)
 ModelCatalog.register_custom_model("hybrid_policy", HybridPolicy)
 ModelCatalog.register_custom_model("fc_policy", FCPolicy)
+
+"""
+not under development: "attention learn to route"-based model
+"""
+#from model.altr_rllib import AltrPolicy
+#ModelCatalog.register_custom_model("altr_policy", AltrPolicy)
