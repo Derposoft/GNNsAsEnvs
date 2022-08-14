4c47cb51f49b863f9a00ed6c0438d35a0f4ab197
derposoft <viswanath.chadalapaka@gmail.com>
2022-08-13 20:13:39 -0700

    +styling

--- model/graph_transformer_rllib.py
+++ model/graph_transformer_rllib.py
@@ -1,6 +1,6 @@
-'''
+"""
 base class from https://github.com/ray-project/ray/blob/master/rllib/models/torch/fcnet.py
-'''
+"""
 # RL/AI imports
 import time
 import ray.rllib.models.torch.torch_modelv2 as TMv2
@@ -49,14 +49,14 @@ class GraphTransformerPolicy(TMv2.TorchModelV2, nn.Module):
         no_final_linear = model_config.get("no_final_linear")
         self.vf_share_layers = model_config.get("vf_share_layers") # this is usually 0
         self.free_log_std = model_config.get("free_log_std") # skip worrying about log std
-        #self.use_mean_embed = kwargs['use_mean_embed'] # obs information
+        #self.use_mean_embed = kwargs["use_mean_embed"] # obs information
         self.map = map
-        self.num_red = kwargs['nred']
-        self.num_blue = kwargs['nblue']
-        self.aggregation_fn = kwargs['aggregation_fn']
+        self.num_red = kwargs["nred"]
+        self.num_blue = kwargs["nblue"]
+        self.aggregation_fn = kwargs["aggregation_fn"]
         self_shape, red_shape, blue_shape = env_setup.get_state_shapes(self.map.get_graph_size(), self.num_red, self.num_blue, env_setup.OBS_TOKEN)
         self.obs_shapes = [self_shape, red_shape, blue_shape, self.num_red, self.num_blue]
-        #self.map.g_acs.add_node(0) # dummy node that we'll use later
+        #self.map.g_acs.add_node(0) # dummy node that we"ll use later
 
         # map info
         self.move_map = {} # movement dictionary: d[node][direction] = newnode. newnode is -1 if direction is not possible from node
@@ -64,7 +64,7 @@ class GraphTransformerPolicy(TMv2.TorchModelV2, nn.Module):
             self.move_map[n] = {}
             ms = self.map.g_acs.adj[n]
             for m in ms:
-                dir = ms[m]['action']
+                dir = ms[m]["action"]
                 self.move_map[n][dir] = m
         
         # actor (attention model)
@@ -72,7 +72,7 @@ class GraphTransformerPolicy(TMv2.TorchModelV2, nn.Module):
         self.N_HEADS = 8
         self.HIDDEN_DIM = 8
         self.gats, _, _ = initialize_graph_transformer(
-            GRAPH_OBS_TOKEN['embedding_size'],
+            GRAPH_OBS_TOKEN["embedding_size"],
             L=self.GAT_LAYERS,
             n_heads=self.N_HEADS,
             hidden_dim=self.HIDDEN_DIM,
@@ -109,7 +109,7 @@ class GraphTransformerPolicy(TMv2.TorchModelV2, nn.Module):
         self._last_flat_in = None
 
         count_model_params(self)
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.to(self.device)
         self.cache = {} # minor speedup (~15%) of training
 
@@ -118,7 +118,7 @@ class GraphTransformerPolicy(TMv2.TorchModelV2, nn.Module):
                 state: List[TensorType],
                 seq_lens: TensorType):
         #start_time = time.time()
-        obs = input_dict['obs_flat'].float()
+        obs = input_dict["obs_flat"].float()
         # transform obs to graph
         attention_input = efficient_embed_obs_in_map(obs, self.map, self.obs_shapes)
         agent_nodes = [get_loc(gx, self.map.get_graph_size()) for gx in obs]
@@ -132,17 +132,17 @@ class GraphTransformerPolicy(TMv2.TorchModelV2, nn.Module):
             self.cache[len(obs)] = batch_graphs.clone()
         else:
             batch_graphs = self.cache[len(obs)].clone()
-        batch_graphs.ndata['feat'] = attention_input.reshape([-1, GRAPH_OBS_TOKEN['embedding_size']])
+        batch_graphs.ndata["feat"] = attention_input.reshape([-1, GRAPH_OBS_TOKEN["embedding_size"]])
         
         # inference
-        batch_x = batch_graphs.ndata['feat']#, None #batch_graphs.edata['feat']
+        batch_x = batch_graphs.ndata["feat"]#, None #batch_graphs.edata["feat"]
         batch_e, batch_lap_enc, batch_wl_pos_enc = None, None, None
         logits = self.gats.forward(batch_graphs, batch_x, batch_e, batch_lap_enc, batch_wl_pos_enc, agent_nodes, self.move_map)
 
         # return
         self._last_flat_in = obs.reshape(obs.shape[0], -1)
         self._features = logits
-        #print('forward takes', time.time()-start_time)
+        #print("forward takes", time.time()-start_time)
         return logits, state
 
     @override(TMv2.TorchModelV2)
